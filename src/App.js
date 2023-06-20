29202db381faa88f8af7b2049ae0631749d4d03e
Srushtiraj Bhosale <srushtiraj777@gmail.com>
2023-06-20 08:07:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import Axios from 'axios';
 
 function App() {
+  const [ listOfUsers, setListOfUsers] = useState([]);
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [username, setUsername] = useState("");
+
+  useEffect(() => {
+    Axios.get("http://localhost:5000/getUsers")
+    .then((responce) => {
+      setListOfUsers(responce.data);
+    })
+  }, [])
+
+  const createUser = () => {
+    Axios.post("http://localhost:5000/createUser", {
+      name, 
+      age, 
+      username,
+    })
+    .then((responce) => {
+      setListOfUsers([...listOfUsers, {name, age, username }])
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='userDisplay'>
+      {listOfUsers.map((user) => {
+        return (
+          <div>
+            <h3>Name: {user.name}</h3>
+            <h3>Age: {user.age}</h3>
+            <h3>Username: {user.username}</h3>
+          </div>
+        )
+      })}
+      </div>
+
+      <div className='createUser'>
+        <input type='text' 
+        placeholder='Name...' 
+        onChange={(event) => {
+          setName(event.target.value)
+        }}/>
+        <input type='number' 
+        placeholder='Age...'
+        onChange={(event) => {
+          setAge(event.target.value)
+        }}
+        />
+        <input type='text' 
+        placeholder='Username...'
+        onChange={(event) => {
+          setUsername(event.target.value)
+        }}
+        />
+        <button onClick={createUser}>Create User</button>
+      </div>
     </div>
   );
 }
